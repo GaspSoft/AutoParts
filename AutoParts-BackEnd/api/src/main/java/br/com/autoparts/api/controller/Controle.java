04d05fe6f12e6172d1e7ee86db394eb82ad5c622
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-07-26 15:41:12 -0300

    Backend Spring

---
+++ AutoParts-BackEnd/api/src/main/java/br/com/autoparts/api/controller/Controle.java
@@ -0,0 +1,14 @@
+package br.com.autoparts.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controle {
+    @GetMapping("/status")
+    public ResponseEntity<?> status(){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
