a6c3d857bc3e9764b690427ea5d0cb5b46914824
Nicolas <nicolas.rm2005@aluno.ifsc.edu.br>
2023-07-27 19:54:58 -0300

    criação de classes service e repository de funcionario

---
+++ AutoParts-BackEnd/api/src/main/java/br/com/autoparts/api/service/FuncionarioServices.java
@@ -0,0 +1,18 @@
+package br.com.autoparts.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FuncionarioServices {
+    @Autowired
+    private Pessoa pessoa;
+
+
+
+
+
+
+
+
+}
