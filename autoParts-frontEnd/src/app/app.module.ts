4e244fcf8c005fd7feff34ceb80dfe798a8fe2b1
Pedro Henrique Godri <pedro.g2006@aluno.ifsc.edu.br>
2023-07-30 01:53:50 -0300

    rounting

--- autoParts-frontEnd/src/app/app.module.ts
+++ autoParts-frontEnd/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
