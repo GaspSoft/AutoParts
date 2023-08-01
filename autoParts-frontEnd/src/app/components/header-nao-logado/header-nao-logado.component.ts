9cbc3977883f72505a37142ad01fb402273c8b30
Bárbara do Nascimento <barbara.n2005@aluno.ifsc.edu.br>
2023-07-31 21:52:28 -0300

    feat: header não logado

--- autoParts-frontEnd/src/app/components/header-nao-logado/header-nao-logado.component.ts
+++ autoParts-frontEnd/src/app/components/header-nao-logado/header-nao-logado.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header-nao-logado',
+  selector: 'header-nao-logado',
   templateUrl: './header-nao-logado.component.html',
   styleUrls: ['./header-nao-logado.component.scss']
 })
