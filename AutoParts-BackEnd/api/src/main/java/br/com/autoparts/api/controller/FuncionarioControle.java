2fd6c30391fed782909687fad3ab9de5971407bf
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-08-01 16:39:40 -0300

    Resolvendo erro de CORS

--- AutoParts-BackEnd/api/src/main/java/br/com/autoparts/api/controller/FuncionarioControle.java
+++ AutoParts-BackEnd/api/src/main/java/br/com/autoparts/api/controller/FuncionarioControle.java
@@ -9,12 +9,13 @@ import br.com.autoparts.api.service.FuncionarioServico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-
+@CrossOrigin(origins = "*")
 @RestController
 public class FuncionarioControle {
     @Autowired
