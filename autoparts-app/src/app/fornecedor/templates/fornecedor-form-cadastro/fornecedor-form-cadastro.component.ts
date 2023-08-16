c79099be36a00931def8f6ce9fd254d937324e34
Pedro Henrique Godri <pedrogodri@gmail.com>
2023-08-15 21:48:41 -0300

    Feat: CRUD de fornecedor

--- autoparts-app/src/app/fornecedor/templates/fornecedor-form-cadastro/fornecedor-form-cadastro.component.ts
+++ autoparts-app/src/app/fornecedor/templates/fornecedor-form-cadastro/fornecedor-form-cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Fornecedor } from 'src/app/model/fornecedor/fornecedor';
+import { FornecedorService } from 'src/app/services/fornecedor/fornecedor.service';
 
 @Component({
   selector: 'app-fornecedor-form-cadastro',
@@ -6,5 +9,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./fornecedor-form-cadastro.component.scss']
 })
 export class FornecedorFormCadastroComponent {
+  fornecedor: Fornecedor;
+  sucessoFeedback: boolean = false;
+  errorsFeedback?: string = '';
+  id?: number;
 
+  constructor(
+    private service: FornecedorService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+  ) {
+    this.fornecedor = new Fornecedor();
+
+  }
+  ngOnInit(): void {
+    this.id = this.service.getFornecedorId()
+      if (this.id) {
+        this.service.getFuncionarioById(this.id).subscribe(
+          response => {
+            this.fornecedor = response;
+          },
+          errorResponse => {
+            this.fornecedor = new Fornecedor();
+          }
+        );
+      }
+
+    console.log()
+  }
+
+  onSubmit(): void {
+    if (this.id) {
+      this.service.atualizarFornecedor(this.fornecedor).subscribe(
+        resposne => {
+          this.sucessoFeedback = true;
+          this.errorsFeedback = '';
+          setTimeout(() => {
+            this.sucessoFeedback = false;
+          }, 7000);
+        },
+        errorResponse => {
+          this.errorsFeedback = 'Erro ao atualizar o funcionário';
+          console.log(errorResponse)
+        }
+      )
+    } else {
+      this.service.cadastrarFornecedor(this.fornecedor).subscribe(
+        response => {
+          this.sucessoFeedback = true;
+          this.errorsFeedback = '';
+          this.fornecedor = response;
+          setTimeout(() => {
+            this.sucessoFeedback = false;
+          }, 7000);
+        },
+        errorResponse => {
+          this.sucessoFeedback = false;
+          this.errorsFeedback = errorResponse.error.errors;
+        }
+      );
+    }
+  }
+  voltarListagem(): void {
+    this.router.navigate(['fornecedor/listar']);
+  }
 }
