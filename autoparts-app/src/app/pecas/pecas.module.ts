82266d29d37289eb54b858251405451a5680791a
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-08-15 21:02:10 -0300

    Criando pastas peças no angular

---
+++ autoparts-app/src/app/pecas/pecas.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PecasCardsComponent } from './template/pecas-cards/pecas-cards.component';
+
+
+
+@NgModule({
+  declarations: [
+    PecasCardsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PecasModule { }
