3789590574c8d264104c0fab0095770636124a57
Vitor Hugo Schiphorst <vitorhugo.schiphorst@gmail.com>
2023-08-20 00:33:09 -0300

    feat: Estilização da Tela de Cadastramento de Cliente Finalizada

--- autoparts-app/src/app/clientes/page/cliente-page/cliente-page-routing.module.ts
+++ autoparts-app/src/app/clientes/page/cliente-page/cliente-page-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClienteFormCadastroComponent } from '../../templates/cliente-form-cadastro/cliente-form-cadastro.component';
-import { ClienteSobreComponent } from '../../templates//cliente-sobre/cliente-sobre.component';
+import { ClienteSobreComponent } from '../../templates/cliente-sobre/cliente-sobre.component';
+import { ClienteFormPerfilComponent } from '../../templates/cliente-form-perfil/cliente-form-perfil.component';
 
 const routes: Routes = [
   { path: 'cadastrar', component: ClienteFormCadastroComponent },
-  { path: 'perfil', component: ClienteSobreComponent }
+  { path: 'sobre', component: ClienteSobreComponent },
+  { path: 'perfil', component: ClienteFormPerfilComponent }
 ];
 
 @NgModule({
