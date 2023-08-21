ba354df71a888293eaf545ec6551e95b9bf9911d
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-08-21 11:57:45 -0300

    Adicionando vendas

---
+++ AutoParts-BackEnd/api/src/main/java/br/com/autoparts/api/controller/VendaControler.java
@@ -0,0 +1,5 @@
+package br.com.autoparts.api.controller;
+
+public class VendaControler {
+    
+}
--- AutoParts-BackEnd/api/src/main/java/br/com/autoparts/api/model/Vendas.java
+++ AutoParts-BackEnd/api/src/main/java/br/com/autoparts/api/model/Vendas.java
@@ -1,9 +1,12 @@
 package br.com.autoparts.api.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,9 +20,21 @@ public class Vendas {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer vendas_id;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "pecas_id")
     private Integer pecas_id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "pecas_fornecedores_id")
     private Integer pecas_fornecedores_id;
-    private Integer vendedores_id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "funcionario_id")
+    private Integer funcionario_id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "clientes_id")
     private Integer clientes_id;
     
 }
