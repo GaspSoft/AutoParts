c6a31aff5db18f62081c2470d0822b44f681ba49
Pedro Henrique Godri <pedrogodri@gmail.com>
2023-08-22 20:56:45 -0300

    Feat: retirando o ngx-mask

--- autoparts-app/src/app/app.module.ts
+++ autoparts-app/src/app/app.module.ts
@@ -12,7 +12,6 @@ import { FuncionariosService } from './services/funcionario/funcionarios.service
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FornecedorService } from './services/fornecedor/fornecedor.service';
 import { PecaService } from './services/pecas/peca.service';
-import { IConfig } from 'ngx-mask';
 
 @NgModule({
   declarations: [
