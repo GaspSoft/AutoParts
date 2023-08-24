00c1d43be4d67584f2ea711e395a9f1a653182c8
Vitor Hugo Schiphorst <vitorhugo.schiphorst@gmail.com>
2023-08-23 21:26:40 -0300

    feat: Rotas (SinglePage) Navbar Cliente e Inicio da Tela Carrinho

--- autoparts-app/src/app/components/navbar-cliente/navbar-cliente.component.ts
+++ autoparts-app/src/app/components/navbar-cliente/navbar-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'navbar-cliente',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 
 export class NavbarClienteComponent {
+
+  constructor(private router: Router) {
+  }
+
+  linkClienteHome():void {
+    this.router.navigate(['/cliente/home-cliente']);
+  }
+
+  linkClienteCarrinho():void {
+    this.router.navigate(['/cliente/carrinho-cliente']);
+  }
+
+  linkClienteSobre():void {
+    this.router.navigate(['/cliente/sobre-cliente']);
+  }
+
+  linkClientePerfil():void {
+    this.router.navigate(['/cliente/perfil-cliente']);
+  }
+
+  linkClienteAjuda():void {
+    this.router.navigate(['/cliente/ajuda-cliente']);
+  }
 }
