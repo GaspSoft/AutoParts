3a685b853293e575f9618dd66732dd66b60ea308
Pedro Godri <pedrogodri@gmail.com>
2023-09-14 12:06:28 -0300

    Feat: Componentes de detalhes

---
+++ autoparts-app/src/app/funcionarios/templates/funcionario-detalhes/funcionario-detalhes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-detalhes',
+  templateUrl: './funcionario-detalhes.component.html',
+  styleUrls: ['./funcionario-detalhes.component.scss']
+})
+export class FuncionarioDetalhesComponent {
+
+}
---
+++ autoparts-app/src/app/funcionarios/templates/pecas-detalhes/pecas-detalhes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pecas-detalhes',
+  templateUrl: './pecas-detalhes.component.html',
+  styleUrls: ['./pecas-detalhes.component.scss']
+})
+export class PecasDetalhesComponent {
+
+}
