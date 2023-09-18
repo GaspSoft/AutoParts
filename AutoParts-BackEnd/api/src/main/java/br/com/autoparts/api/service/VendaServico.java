946f4ec80deeb12864794feaeafb1393c46261e6
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-09-18 17:17:55 -0300

    Mudando classe back e criando service e model de venda front

--- AutoParts-BackEnd/api/src/main/java/br/com/autoparts/api/service/VendaServico.java
+++ AutoParts-BackEnd/api/src/main/java/br/com/autoparts/api/service/VendaServico.java
@@ -21,7 +21,6 @@ import br.com.autoparts.api.repository.FuncionarioRepositorio;
 import br.com.autoparts.api.repository.PecasRepositorio;
 import br.com.autoparts.api.repository.VendasRepositorio;
 
- 
 @Service
 public class VendaServico {
     @Autowired
@@ -32,90 +31,71 @@ public class VendaServico {
 
     @Autowired
     private PecasServico pecaServico;
+    
     @Autowired
     private PecasRepositorio pecaRepositorio;
 
     @Autowired
-    private ClienteServico clienteServico;
-    @Autowired
     private ClienteRepositorio clienteRepositorio;
 
-    @Autowired
-    private FuncionarioServico funcionarioServico;
-    @Autowired
-    private FuncionarioRepositorio funcionarioRepositorio;
 
-    @Autowired
-    private FornecedorServico fornecedorServico;
     @Autowired
     private FornecedorRepositorio fornecedorRepositorio;
 
     public ResponseEntity<?> cadastrarVenda(Vendas venda) {
-    
-            if (
-                venda.getPeca().getFornecedor().getFornecedor_id()== null
-                || venda.getCliente().getCliente_id() == null
-                || venda.getFuncionario().getFuncionario_id() == null
-                || venda.getPeca().getPecas_id() == null
-                
-            ) {
-                retorno.setMensagem("Um ou mais campos da venda são nulos.");
-                return ResponseEntity.badRequest().body(retorno);
-            }
-        
-        // Verifica se o cliente, funcionário, peça e fornecedor existem e o estoque da peça
-        
-            Optional<Cliente> clienteExist = clienteRepositorio.findById(venda.getCliente().getCliente_id());
-            Optional<Fornecedor> fornecedorExist = fornecedorRepositorio.findById(venda.getPeca().getFornecedor().getFornecedor_id());
-            Optional<Funcionario> funcionarioExist = funcionarioRepositorio.findById(venda.getFuncionario().getFuncionario_id());
-            Optional<Pecas> pecaExist = pecaRepositorio.findById(venda.getPeca().getPecas_id());
-            if (!clienteExist.isPresent()){
-                retorno.setMensagem("Cliente não encontrado.");
-                return ResponseEntity.badRequest().body(retorno);
-            }
-            else{
-                venda.setCliente(clienteExist.get());
-            }
-            if(!fornecedorExist.isPresent()){
-                retorno.setMensagem("Fornecedor não encontrado.");
-                return ResponseEntity.badRequest().body(retorno);
-            }
-            else{
-                venda.getPeca().setFornecedor(fornecedorExist.get());
-            }
-            if(!funcionarioExist.isPresent()){
-                retorno.setMensagem("Funcionário não encontrado.");
-                return ResponseEntity.badRequest().body(retorno);
-            }
-            else{
-                venda.setFuncionario(funcionarioExist.get());
-            }
-            if( !pecaExist.isPresent()){
-                retorno.setMensagem("Peça não encontrada.");
-                return ResponseEntity.badRequest().body(retorno);
-            }
-            else{
-                venda.setPeca(pecaExist.get());
-            }
-            if(venda.getPeca().getQuantidade() <= 0){
-                retorno.setMensagem("Peças sem estoque.");
-                return ResponseEntity.badRequest().body(retorno);
-            }
 
-        
+        if (venda.getPeca().getFornecedor().getFornecedor_id() == null
+                || venda.getCliente().getCliente_id() == null
+                || venda.getPeca().getPecas_id() == null
+
+        ) {
+            retorno.setMensagem("Um ou mais campos da venda são nulos.");
+            return ResponseEntity.badRequest().body(retorno);
+        }
+
+        // Verifica se o cliente, funcionário, peça e fornecedor existem e o estoque da
+        // peça
+
+        Optional<Cliente> clienteExist = clienteRepositorio.findById(venda.getCliente().getCliente_id());
+        Optional<Fornecedor> fornecedorExist = fornecedorRepositorio
+                .findById(venda.getPeca().getFornecedor().getFornecedor_id());
+        Optional<Pecas> pecaExist = pecaRepositorio.findById(venda.getPeca().getPecas_id());
+        if (!clienteExist.isPresent()) {
+            retorno.setMensagem("Cliente não encontrado.");
+            return ResponseEntity.badRequest().body(retorno);
+        } else {
+            venda.setCliente(clienteExist.get());
+        }
+        if (!fornecedorExist.isPresent()) {
+            retorno.setMensagem("Fornecedor não encontrado.");
+            return ResponseEntity.badRequest().body(retorno);
+        } else {
+            venda.getPeca().setFornecedor(fornecedorExist.get());
+        }
+        if (!pecaExist.isPresent()) {
+            retorno.setMensagem("Peça não encontrada.");
+            return ResponseEntity.badRequest().body(retorno);
+        } else {
+            venda.setPeca(pecaExist.get());
+        }
+        if (venda.getPeca().getQuantidade() <= 0) {
+            retorno.setMensagem("Peças sem estoque.");
+            return ResponseEntity.badRequest().body(retorno);
+        }
+
         // Diminui o estoque da peça
-        
+
         pecaServico.diminuirEstoque(venda.getPeca().getQuantidade(), venda.getPeca().getPecas_id());
-        
+
         // Salva a venda no banco de dados
         vendaRepositorio.save(venda);
         retorno.setMensagem("Venda(s) cadastrada(s) com sucesso.");
         return new ResponseEntity<>(retorno, HttpStatus.CREATED);
     }
 
-    public ResponseEntity<?> listarVendas(){
+    public ResponseEntity<?> listarVendas() {
         List<Vendas> vendas = vendaRepositorio.findAll();
-        if(vendas.isEmpty()){
+        if (vendas.isEmpty()) {
             retorno.setMensagem("Não há vendas cadastradas.");
             return ResponseEntity.badRequest().body(retorno);
         }
