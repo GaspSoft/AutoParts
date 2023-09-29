239f5657928be7df13c66b0fd13de8853eb9f7ad
Nicolas Ricardo Maba <nicolas.rm2005@aluno.ifsc.edu.br>
2023-09-28 22:26:56 -0300

    consumindo a api de login

--- autoparts-app/src/app/services/login/login.service.ts
+++ autoparts-app/src/app/services/login/login.service.ts
@@ -1,27 +1,18 @@
-  import { HttpClient, HttpHeaders } from '@angular/common/http';
-  import { Injectable } from '@angular/core';
-  import { Observable } from 'rxjs';
-  import { Pessoa } from 'src/app/model/pessoa/pessoa';
 
-  @Injectable({
-    providedIn: 'root'
-  })
-  export class LoginService {
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pessoa } from 'src/app/model/pessoa/pessoa';
 
-    constructor(private http: HttpClient) { }
-    private url: string = 'http://localhost:8080/login';
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
 
-      login(email: string, senha: string): Observable<any> {
-    const requestBody = { email: email, senha: senha };
+  constructor(private http: HttpClient) { }
+  private url: string = 'http://localhost:8080/login';
 
-    // Defina os cabeçalhos, incluindo o cabeçalho de autorização
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
- 
-    });
-
-    // Envia a solicitação POST com email, senha e cabeçalhos configurados
-    return this.http.post(this.url, requestBody, { headers: headers });
+  login(pessoa: Pessoa): Observable<HttpResponse<any>> {
+    return this.http.post<any>(this.url, pessoa, { observe: 'response' });
   }
 }
-  
