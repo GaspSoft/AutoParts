43e1f959c87a479af629f759e0f1b9509584c0b5
Vitor Hugo Schiphorst <vitorhugo.schiphorst@gmail.com>
2023-10-03 23:00:05 -0300

    feat: Button Logoff e Dark/Light Mode

--- autoparts-app/src/app/components/navbar-cliente/navbar-cliente.component.ts
+++ autoparts-app/src/app/components/navbar-cliente/navbar-cliente.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Etheme } from 'src/app/enums/Ethemes.enum';
 
 @Component({
   selector: 'navbar-cliente',
@@ -8,6 +9,9 @@ import { Router } from '@angular/router';
 })
 
 export class NavbarClienteComponent {
+  public iconTheme: string = Etheme.ICON_MOON;
+  public textTheme: string = Etheme.TEXT_MOON;
+  public body = document.body.classList.toggle('light-theme');
 
   constructor(private router: Router) {
   }
@@ -34,5 +38,21 @@ export class NavbarClienteComponent {
 
   linkClienteAjuda():void {
     this.router.navigate(['/cliente/ajuda']);
+  }
+
+  public toggle() {
+    const body = document.body;
+
+    if (body.classList.contains('light-theme')) {
+      body.classList.remove('light-theme');
+      body.classList.add('dark-theme');
+      this.textTheme = Etheme.TEXT_MOON;
+      this.iconTheme = Etheme.ICON_MOON;
+    } else if (body.classList.contains('dark-theme')) {
+      body.classList.remove('dark-theme');
+      body.classList.add('light-theme');
+      this.textTheme = Etheme.TEXT_SUN;
+      this.iconTheme = Etheme.ICON_SUN;
+    }
   }
 }
---
+++ autoparts-app/src/app/enums/Ethemes.enum.ts
@@ -0,0 +1,6 @@
+export enum Etheme {
+    ICON_MOON = 'bi-moon-fill',
+    TEXT_MOON = 'Dark Theme',
+    ICON_SUN = 'bi-sun-fill',
+    TEXT_SUN = 'Light Theme',
+}
