b5dfd347410d7a70ad65272bc7a274b795f2b60c
Vitor Hugo Schiphorst <vitorhugo.schiphorst@gmail.com>
2023-10-16 18:47:32 -0300

    Conflito...

--- autoparts-app/src/app/clientes/page/cliente-page/cliente-page.module.ts
+++ autoparts-app/src/app/clientes/page/cliente-page/cliente-page.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientePageRoutingModule } from './cliente-page-routing.module';
-import { ClientesModule } from '../../clientes.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ClientePageRoutingModule,
-    ClientesModule
+    ClientePageRoutingModule
   ]
 })
 export class ClientePageModule { }
