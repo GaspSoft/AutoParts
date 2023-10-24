c5d0c0e6fd7b0d3855fc8853e0afd6b3e3b1e41f
Rafael <rafael.e01@aluno.ifsc.edu.br>
2023-10-24 11:20:26 -0300

    Retaforando mer e der

--- Autoparts-DB/scripts_autoparts.sql
+++ Autoparts-DB/scripts_autoparts.sql
@@ -1,82 +1,104 @@
--- Exclui o Banco
--- DROP DATABASE autoparts_db;
+-- MySQL Workbench Forward Engineering
+-- -----------------------------------------------------
+-- Schema autoparts_db
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `autoparts_db` ;
 
-CREATE DATABASE IF NOT EXISTS autoparts_db;
+-- -----------------------------------------------------
+-- Schema autoparts_db
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `autoparts_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `autoparts_db` ;
 
-USE autoparts_db;
+-- -----------------------------------------------------
+-- Table `autoparts_db`.`enderecos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `autoparts_db`.`enderecos` (
+  `endereco_id` INT NOT NULL AUTO_INCREMENT,
+  `bairro` VARCHAR(255) NULL DEFAULT NULL,
+  `cep` BIGINT NULL DEFAULT NULL,
+  `cidade` VARCHAR(255) NULL DEFAULT NULL,
+  `complemento` VARCHAR(255) NULL DEFAULT NULL,
+  `estado` VARCHAR(255) NULL DEFAULT NULL,
+  `numero` INT NULL DEFAULT NULL,
+  `rua` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`endereco_id`));
 
-CREATE TABLE IF NOT EXISTS `enderecos` (
-  `endereco_id` INT AUTO_INCREMENT NOT NULL,
-  `cep` BIGINT NOT NULL,
-  `estado` VARCHAR(45) NOT NULL,
-  `cidade` VARCHAR(45) NOT NULL,
-  `bairro` VARCHAR(45) NOT NULL,
-  `rua` VARCHAR(45) NOT NULL,
-  `numero` INT NOT NULL,
-  `complemento` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`endereco_id`)
-);
 
 
-CREATE TABLE IF NOT EXISTS `fornecedores` (
-  `fornecedor_id` INT AUTO_INCREMENT  NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `cnpj` bigint not null,
-  PRIMARY KEY (`fornecedor_id`)
-);
+-- -----------------------------------------------------
+-- Table `autoparts_db`.`clientes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `autoparts_db`.`clientes` (
+  `cliente_id` INT NOT NULL AUTO_INCREMENT,
+  `cpf` BIGINT NULL DEFAULT NULL,
+  `email` VARCHAR(255) NULL DEFAULT NULL,
+  `nome` VARCHAR(255) NULL DEFAULT NULL,
+  `senha` VARCHAR(255) NULL DEFAULT NULL,
+  `tipo_pessoa` TINYINT NULL DEFAULT NULL,
+  `enderecos_id` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`cliente_id`),
+  UNIQUE INDEX `UK_miodm1hrkbbj5ec28jbnt4mnk` (`enderecos_id` ASC) VISIBLE,
+    FOREIGN KEY (`enderecos_id`)
+    REFERENCES `autoparts_db`.`enderecos` (`endereco_id`));
 
-CREATE TABLE IF NOT EXISTS `pecas` (
-  `pecas_id` INT  AUTO_INCREMENT NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `descricao` VARCHAR(45) NOT NULL,
-  `quantidade` INT NOT NULL,
-  `foto` BLOB NOT NULL,
-  `marca` VARCHAR(45) NOT NULL,
-  `ano` INT NOT NULL,
-  `preco` DOUBLE NOT NULL,
-  `modelo` VARCHAR(45) NOT NULL,
-  `fornecedores_id` INT NOT NULL,
-  PRIMARY KEY (`pecas_id`, `fornecedores_id`),
-  CONSTRAINT `fk_pecas_fornecedores1` FOREIGN KEY (`fornecedores_id`) REFERENCES `fornecedores` (`fornecedor_id`)
-	ON DELETE CASCADE
-);
 
-CREATE TABLE IF NOT EXISTS `clientes` (
-  `cliente_id` INT auto_increment NOT NULL,
-  `cpf` bigint NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `senha` VARCHAR(50) NOT NULL,
-  `enderecos_id` INT NOT NULL,
-  `tipo_pessoa` tinyInt NOT NULL,
-  PRIMARY KEY (`cliente_id`, `enderecos_id`),
-  CONSTRAINT `fk_clientes_enderecos1` FOREIGN KEY (`enderecos_id`) REFERENCES `enderecos` (`endereco_id`)
-  ON DELETE CASCADE
-);
+-- -----------------------------------------------------
+-- Table `autoparts_db`.`fornecedores`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `autoparts_db`.`fornecedores` (
+  `fornecedor_id` INT NOT NULL AUTO_INCREMENT,
+  `cnpj` BIGINT NULL DEFAULT NULL,
+  `nome` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`fornecedor_id`));
 
-CREATE TABLE IF NOT EXISTS `funcionarios` (
-  `funcionario_id` INT AUTO_INCREMENT NOT NULL,
-  `cpf` bigint NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `senha` varchar(50) NOT NULL,
-  `tipo_pessoa` tinyInt NOT NULL,
-  PRIMARY KEY (`funcionario_id`)
 
-);
+-- -----------------------------------------------------
+-- Table `autoparts_db`.`funcionarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `autoparts_db`.`funcionarios` (
+  `funcionario_id` INT NOT NULL AUTO_INCREMENT,
+  `cpf` BIGINT NULL DEFAULT NULL,
+  `email` VARCHAR(255) NULL DEFAULT NULL,
+  `nome` VARCHAR(255) NULL DEFAULT NULL,
+  `senha` VARCHAR(255) NULL DEFAULT NULL,
+  `tipo_pessoa` TINYINT NULL DEFAULT NULL,
+  PRIMARY KEY (`funcionario_id`));
 
-CREATE TABLE IF NOT EXISTS `vendas` (
-  `vendas_id` INT AUTO_INCREMENT NOT NULL,
-  `pecas_id` INT NOT NULL,
-  `pecas_fornecedores_id` INT NOT NULL,
-  `vendedores_id` INT NOT NULL,
-  `clientes_id` INT NOT NULL,
-  
 
-  PRIMARY KEY (`vendas_id`, `pecas_id`, `pecas_fornecedores_id`, `vendedores_id`, `clientes_id`),
-  CONSTRAINT `fk_pecas_has_vendedores_pecas1` FOREIGN KEY (`pecas_id`, `pecas_fornecedores_id`) REFERENCES `pecas` (`pecas_id`, `fornecedores_id`),
-  CONSTRAINT `fk_vendas_vendedores1` FOREIGN KEY (`vendedores_id`) REFERENCES `funcionarios` (`funcionario_id`),
-  CONSTRAINT `fk_vendas_clientes1` FOREIGN KEY (`clientes_id`) REFERENCES `clientes` (`cliente_id`)
-  ON DELETE CASCADE
-);
+-- -----------------------------------------------------
+-- Table `autoparts_db`.`pecas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `autoparts_db`.`pecas` (
+  `pecas_id` INT NOT NULL AUTO_INCREMENT,
+  `ano` INT NULL DEFAULT NULL,
+  `base64` VARCHAR(255) NULL DEFAULT NULL,
+  `descricao` VARCHAR(255) NULL DEFAULT NULL,
+  `foto` LONGBLOB NULL DEFAULT NULL,
+  `marca` VARCHAR(255) NULL DEFAULT NULL,
+  `modelo` VARCHAR(255) NULL DEFAULT NULL,
+  `nome` VARCHAR(255) NULL DEFAULT NULL,
+  `preco` DOUBLE NULL DEFAULT NULL,
+  `quantidade` INT NULL DEFAULT NULL,
+  `fornecedor_id` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`pecas_id`),
+  INDEX `FK4hdxrch99lqiu64lgf07uvtxd` (`fornecedor_id` ASC) VISIBLE,
+    FOREIGN KEY (`fornecedor_id`)
+    REFERENCES `autoparts_db`.`fornecedores` (`fornecedor_id`));
+
+
+-- -----------------------------------------------------
+-- Table `autoparts_db`.`vendas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `autoparts_db`.`vendas` (
+  `vendas_id` INT NOT NULL AUTO_INCREMENT,
+  `cliente_id` INT NULL DEFAULT NULL,
+  `pecas_id` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`vendas_id`),
+  INDEX `FK18jb6atkgnouxy9b97unm4uue` (`cliente_id` ASC) VISIBLE,
+  INDEX `FKdcp3au3luq74xthyhgt5a30e4` (`pecas_id` ASC) VISIBLE,
+    FOREIGN KEY (`cliente_id`)
+    REFERENCES `autoparts_db`.`clientes` (`cliente_id`),
+    FOREIGN KEY (`pecas_id`)
+    REFERENCES `autoparts_db`.`pecas` (`pecas_id`));
 
